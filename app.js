9255bea780b29c516795e924c26ecc50a104c32b
Philip <minielly5@hotmail.com>
2021-02-16 10:16:51 -0500

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const HttpError = require("./models/http-error");
 const placesRoutes = require("./routes/places-routes");
@@ -14,19 +15,10 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRouter);
